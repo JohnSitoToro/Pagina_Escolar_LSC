c39869b22edd5ad58e4a38e89ac0ae677a91d164
JohnSitoToro <johnsito.toro13@gmail.com>
2025-09-13 21:53:45 -0500

    Creacion

---
+++ JAVASCRIPT/scriptMain.js
@@ -0,0 +1,7 @@
+function abrirLightbox(id) {
+  document.getElementById(id).classList.add("activo");
+}
+function cerrarLightbox(id) {
+  document.getElementById(id).classList.remove("activo");
+}
+
